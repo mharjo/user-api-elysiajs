645e2f2e5db0ff56314de58ba970905d746395c3
Misni Harjo <masbejox@gmail.com>
2026-04-08 19:54:02 +0700

    feat: add comprehensive unit tests for all user APIs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Elysia } from "elysia";
 import { usersRoute } from "./routes/users-route";
 
-const app = new Elysia()
+export const app = new Elysia()
   .use(usersRoute)
-  .get("/", () => ({ status: "ok", message: "Elysia + Drizzle + MySQL is running" }))
-  .listen(3000);
+  .get("/", () => ({ status: "ok", message: "Elysia + Drizzle + MySQL is running" }));
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
